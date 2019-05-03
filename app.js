edaf798a9f33e50ce0adb533c2e4f6b3904872bc
Beno Suriano <bjsuriano@gmail.com>
2019-05-03 12:13:35 -0600

    Defined models.

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express()
 const morgan = require('morgan')
 const path = require('path')
 const layout = require('./views/layout')
+const PORT = 1337
 
+const {db, Page, User} = require('./models')
 
+const init = async () => {
+  await db.sync({force: true})
+
+  app.listen(PORT, console.log(`Server listening on port ${PORT}`))
+}
+
+db.authenticate().
+then(() => {
+  console.log('connected to the db')
+})
 
 app.use(morgan('dev'))
 app.use(express.urlencoded( { extended:false } ))
@@ -14,7 +26,7 @@ app.get('/', (req,res,next)=> {
   res.send(layout('Empty string of nothing'))
 })
 
-const PORT = 1337
+init()
 
-app.listen(PORT, console.log(`Server listening on port ${PORT}`))
+
 
--- models/index.js
+++ models/index.js
@@ -1,5 +1,36 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/wikistack2')
+const db = new Sequelize('postgres://localhost:5432/wikistack2',{logging: false})
 
-module.exports = { db }
+const Page = db.define('pages', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  status: Sequelize.ENUM('open', 'closed')
+})
+
+const User = db.define('users', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    isUnique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
+
+module.exports = { db, Page, User }
 
